2022da4ea13cd2b23052ce849439b657ef939cf3
Najwan Aufa <114733534+Najwan-Aufa@users.noreply.github.com>
2024-09-20 16:29:31 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ const navItems = document.querySelectorAll('.nav-item');
 // Tambahkan event listener untuk setiap item navigasi
 navItems.forEach(item => {
   item.addEventListener('click', function() {
-    // Hapus class "active" dari semua item
+  
     navItems.forEach(nav => nav.classList.remove('active'));
 
-    // Tambahkan class "active" ke item yang diklik
+   
     this.classList.add('active');
   });
 });
+
+const menuItems = document.querySelectorAll('.nav-item');
+
+  
+  menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+    
+      document.getElementById('nav_check').checked = false;
+    });
+  });
